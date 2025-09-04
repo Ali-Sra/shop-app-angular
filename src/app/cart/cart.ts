6b63c8789e1bfdf31fc09dcaf1963bf42a11f87c
Ali-Sra <150520460+Ali-Sra@users.noreply.github.com>
2025-09-04 16:17:59 +0200

    Update: Änderungen im Cart und neues my-card Component hinzugefügt

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CurrencyPipe, NgFor, NgIf],
+  imports: [CurrencyPipe],
   templateUrl: './cart.html',
   styleUrls: ['./cart.css']
 })
